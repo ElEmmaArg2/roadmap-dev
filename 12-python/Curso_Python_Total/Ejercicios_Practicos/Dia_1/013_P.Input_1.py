faace414309eb07e5c10a856b0e7655786a5d7f0
Franco E. Cardoso <emmanuelcardosoahz@gmail.com>
2025-10-24 13:24:03 -0300

    Practica de Python inicial

---
+++ 12-python/Curso_Python_Total/Ejercicios_Practicos/Dia_1/013_P.Input_1.py
@@ -0,0 +1,7 @@
+nombre = input("Dime tu nombre: ")
+apellido = input("Dume tu apellido: ")
+
+print("Mi nombre es " + nombre + " y mi apellido es " + apellido)
+
+#O Tambien podemos poner
+print("Tu nombre es " + input("Dime tu nombre: ")+ " " + input("Dime tu apellido: "))
---
+++ 12-python/Curso_Python_Total/Hola_Mundo.py
@@ -0,0 +1,8 @@
+print("Hola mundo, te digo 'Hola'")
+print(100+50)
+
+print("Hola" + " Fede")
+
+print("Me llamo \"Federico\"")
+
+print("Esta es una linea \nY esta es otra linea")
